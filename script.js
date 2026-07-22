64b5c29118eb20170264f89d42c33df5641a69c8
Zyadwn <ymjjvg6p62@privaterelay.appleid.com>
2026-07-22 12:59:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("FOCC Dashboard Started");
+function updateClock() {
+
+const now = new Date();
+
+document.getElementById("localTime").innerHTML =
+now.toLocaleTimeString("en-GB", {
+hour: "2-digit",
+minute: "2-digit"
+});
+
+document.getElementById("zuluTime").innerHTML =
+now.toLocaleTimeString("en-GB", {
+timeZone: "UTC",
+hour: "2-digit",
+minute: "2-digit"
+});
+
+document.getElementById("gregorianDate").innerHTML =
+now.toLocaleDateString("en-GB", {
+weekday: "long",
+day: "numeric",
+month: "long",
+year: "numeric"
+});
+
+document.getElementById("hijriDate").innerHTML =
+new Intl.DateTimeFormat(
+'en-TN-u-ca-islamic',
+{
+day:'numeric',
+month:'long',
+year:'numeric'
+}
+).format(now);
+
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
